8954c2bcaa8c57549e58e2d1cacef06ac262b3c3
Sonjoo <joohotheman@gmail.com>
2019-10-05 20:32:25 +0900

    add CouponController

--- app/Http/Controllers/CouponController.php
+++ app/Http/Controllers/CouponController.php
@@ -4,10 +4,30 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Services\CouponService;
+
+use App\Dtos\CouponDTO;
+
 use App\Coupon;
 
 class CouponController extends Controller
 {
+    private $couponService;
+
+    public function __construct(CouponService $couponService) {
+      $this->couponService = $couponService;
+    }
+
+    public function createCoupons(Request $request) {
+      $couponInfo = new CouponDTO($request->input('prefix'), $request->input('group_name'), $request->input('length', 16));
+      $is_success = $this->couponService($couponInfo);
+      if ($is_success) {
+        return ['success' => true];
+      } else {
+        return ['success' => false];
+      }
+    }
+
     public function getCouponList(Request $request) {
       $coupons = null;
       if ($request->input('group_id') !== null) {
@@ -15,5 +35,6 @@ class CouponController extends Controller
       } else {
         $coupons = Coupon::paginate(100);
       }
+      return $coupons;
     }
 }
